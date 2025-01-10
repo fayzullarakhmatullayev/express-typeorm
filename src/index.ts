46762cb9defa82fb28237a59b14d8fd0332dc2a3
Fayzulla <fayzullarakhmatullayev@gmail.com>
2025-01-10 16:13:03 +0500

    refactor server initialization to use app module and streamline imports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import { Request, Response } from "express";
-import * as morgan from "morgan";
-import "dotenv/config";
+import app from "./app";
 import { AppDataSource } from "./data-source";
 
-
 AppDataSource.initialize()
-
-
-
+  .then(async () => {
+    const PORT = process.env.PORT || 8080;
 
     app.listen(PORT, () => {
       console.log(`Express server has started on http://localhost:${PORT}`);
